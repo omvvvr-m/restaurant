9af4c37f14feb6775b96bdc59dc73df57d4892f9
M-R444 <hksj690@gmail.com>
2025-04-25 22:05:30 +0300

    js

---
+++ menu2/js/pimp.js
@@ -0,0 +1,47 @@
+function increase(btn){
+    let qs= btn.parentElement.querySelector(".q");
+    let a= parseInt(qs.textContent);
+    a++;
+    qs.textContent=a;
+    return qs.textContent;
+    // let quantity=qs.textContent;
+    // let value=a;
+    // localStorage.setItem(quantity,a);
+    // alert("added");
+    // localStorage.getItem(quantity,a);
+    // console.log(localStorage.getItem(quantity,a));
+}
+function decrease(btn){
+    let qs= btn.parentElement.querySelector(".q");
+    let a= parseInt(qs.textContent);
+    if(a>0)
+    a--;
+    qs.textContent=a;
+    return qs.textContent;
+    // let quantity=qs.textContent;
+    // let value=a;
+    // localStorage.removeItem(quantity,a);
+    //  alert("deleted");
+}
+function save (btn){
+    let card=btn.closest(".ph1");
+    let name=card.querySelector(".n1").textContent;
+    let price=parseInt(card.querySelector(".p1").textContent);
+    let quantity=parseInt(card.querySelector(".q").textContent);
+    let neworder={
+        name,price,quantity
+    }
+    let jsonorder=JSON.stringify(neworder);
+    let c=JSON.parse(localStorage.getItem("orders"));
+    // let cindex=c.findIndex(order=>order.name===name);
+    // if(cindex!==-1){
+        // c[cindex]=neworder;
+    // }
+    // else{
+        // c.push(neworder);
+    // }
+    localStorage.setItem("order",JSON.stringify(neworder));
+    localStorage.getItem("order",JSON.stringify(neworder));
+    console.log(neworder);
+
+}
