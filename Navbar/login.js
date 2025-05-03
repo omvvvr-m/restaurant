bf9b23674dc314e331b93c4dea7f98eadc2a86cd
baad <dell1876d@gmail.com>
2025-05-04 01:19:02 +0300

    انا ليه

---
+++ Navbar/login.js
@@ -0,0 +1,10 @@
+let isLoggedIn = false;
+
+const btn = document.getElementById("submitLogin");
+if (btn) {
+
+    btn.addEventListener("click", () => {
+        console.log("hello world");
+
+    });
+}
