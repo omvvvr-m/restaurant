24be9b3df4772f9f7953ea8bc266340e8a62544d
mariem294 <mariemhemdan795@gmail.com>
2025-05-04 01:43:20 +0300

    offer

--- menu/script.js
+++ menu/script.js
@@ -17,3 +17,54 @@ document.querySelectorAll('.btm2').forEach((button) => {
       }
     });
   });
+
+
+  //----------------order----------------//
+
+  window.addEventListener("DOMContentLoaded",function(){
+    let cards=
+    document.querySelectorAll(".offer_card");
+    cards.forEach(function(card){
+      let tittle=
+      card.querySelector(".offer_tittle");
+      let count =
+      card.querySelector(".count");
+      console.log("Meal:",tittle.innerText)
+      console.log("quantity:",count.value);
+
+        });
+
+      let orderbtn=
+      document.querySelector(".offer_btn");
+      orderbtn.addEventListener("click",function() {
+        let selectmeals =[];
+        cards.forEach(function(card){
+          let tittle=
+          card.querySelector(".offer_tittle");
+          let count =
+          card.querySelector(".count");
+          
+          let qty=
+          parseInt(count.value);   
+          if(qty>0){
+            selectmeals.push({
+              name:tittle.innerText,
+              quantity:qty
+            });
+          }
+
+
+        });
+        localStorage.setItem("meals",JSON.stringify(selectmeals));
+        
+            JSON.parse(localStorage.getItem("meals"))
+        window.location.href="../order/order.html";
+
+
+      });
+
+    });
+ 
+ 
+  
+  
