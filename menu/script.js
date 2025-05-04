39b7f3ece436dc586038bb76177d9cc301a6da9e
mariem294 <mariemhemdan795@gmail.com>
2025-05-04 21:23:03 +0300

    Update script.js

--- menu/script.js
+++ menu/script.js
@@ -21,7 +21,7 @@ document.querySelectorAll('.btm2').forEach((button) => {
 
   //----------------order----------------//
 
-  window.addEventListener("DOMContentLoaded",function(){
+ /* window.addEventListener("DOMContentLoaded",function(){
     let cards=
     document.querySelectorAll(".offer_card");
     cards.forEach(function(card){
@@ -63,8 +63,31 @@ document.querySelectorAll('.btm2').forEach((button) => {
 
       });
 
-    });
- 
- 
-  
-  
+    });*/
+    //----------------------------------------------------------------//
+
+  window.addEventListener("DOMContentLoaded",function(){
+    const orderbutton=
+      document.querySelector(".offer_btn");
+      orderbutton.addEventListener("click",function(){
+        const alloffers=
+        document.querySelectorAll(".offer_card");
+       const orders=[];
+       alloffers.forEach(offer=>{
+        const name=offer.querySelector(".offer_tittle").textContent;
+        const quantity=offer.querySelector(".count").textContent;
+        const price=offer.querySelector(".new").textContent;
+        if(parseInt(quantity)>0){
+
+          orders.push({name ,quantity,price});
+
+        }
+      });
+
+      const orderstring=
+        encodeURIComponent(JSON.stringify(orders));
+       window.location.href=`order.html?data=${orderstring}`;
+
+
+      });
+  });
