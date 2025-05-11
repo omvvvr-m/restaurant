3c47c7cf69f96e07e3bb4077c4fc7b2a23dc9c3d
M-R444 <hksj690@gmail.com>
2025-05-11 07:47:55 +0300

    d

--- menu2/js/pimp.js
+++ menu2/js/pimp.js
@@ -4,12 +4,6 @@ function increase(btn){
     a++;
     qs.textContent=a;
     return qs.textContent;
-    // let quantity=qs.textContent;
-    // let value=a;
-    // localStorage.setItem(quantity,a);
-    // alert("added");
-    // localStorage.getItem(quantity,a);
-    // console.log(localStorage.getItem(quantity,a));
 }
 function decrease(btn){
     let qs= btn.parentElement.querySelector(".q");
@@ -18,10 +12,6 @@ function decrease(btn){
     a--;
     qs.textContent=a;
     return qs.textContent;
-    // let quantity=qs.textContent;
-    // let value=a;
-    // localStorage.removeItem(quantity,a);
-    //  alert("deleted");
 }
 function save (btn){
     let card=btn.closest(".ph1");
@@ -32,24 +22,13 @@ function save (btn){
         name,price,quantity
     }
     let jsonorder=JSON.stringify(neworder);
-    // let c=JSON.parse(localStorage.getItem("orders"));
-    // let cindex=c.findIndex(order=>order.name===name);
-    // if(cindex!==-1){
-        // c[cindex]=neworder;
-    // }
-    // else{
-        // c.push(neworder);
-    // }
     localStorage.setItem("order",JSON.stringify(neworder));
     // localStorage.getItem("order",JSON.stringify(neworder));
      window.location.href= "../order/order.html";
-    console.log(neworder);
+    // console.log(neworder);
 }
 
-// function Go (){
-//     window.location.href= "../order/order.html"
-// function Go (){
-//     window.location.href= "../order/order.html"}
+
 
 
 
